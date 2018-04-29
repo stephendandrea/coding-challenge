d838981ef783b6fd1b1682a33ffad093315475ff
David Coleman <david_coleman_007@hotmail.com>
2018-04-28 20:04:50 -0400

    a last few linting issues

--- app/components/ActivityCell/ActivityCell.js
+++ app/components/ActivityCell/ActivityCell.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './ActivityCell.scss';
 
-export const ActivityCell = (props) => {
-  const {density} = props;
+export const ActivityCell = props => {
+  const { density } = props;
   const cellClasses = `cell activity-density-${density}`;
   return (
     <li className={cellClasses}/>
-  )
-}
+  );
+};
