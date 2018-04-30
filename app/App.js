e580d455ac791a45a7f93532b9deb166484b2c3c
Stephen D'Andrea <sadandrea@gmail.com>
2018-04-30 12:27:23 -0400

    Instructions

--- app/App.js
+++ app/App.js
@@ -6,15 +6,24 @@ export default class App extends React.Component {
   render () {
 
     return (
-      <React.Fragment>
+      <div className="container">
         <h2>Coding Challenge</h2>
-        <p>Recreate this graph:</p>
         <img src="/images/git-graph.gif" alt="Git Graph" className="git-graph"/>
-        <p>Using this data:</p>
-        <a href="https://api.github.com/repos/facebook/react/stats/commit_activity">
-          https://api.github.com/repos/facebook/react/stats/commit_activity
-        </a>
-      </React.Fragment>
+        <p>
+          Create the graph above using this data:
+          <br/>
+          <a href="https://api.github.com/repos/facebook/react/stats/commit_activity">https://api.github.com/repos/facebook/react/stats/commit_activity</a>
+        </p>
+        <h3>Notes:</h3>
+        <ul>
+          <li>
+            Commit color density should be relative to the highest single day activity and broken into 4 quarters.
+          </li>
+          <li>
+            If the highest single days activity is 84, any day in the top 25% (63-84 commits) should have the highest density color.
+          </li>
+        </ul>
+      </div>
     );
 
   }
